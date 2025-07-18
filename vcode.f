d22c222feb73ad0a03845a2ba2cabfe718d84cde
hxxdev <hxx.dev@gmail.com>
2025-07-19 01:12:49 +0900

    fixed paths

--- vcode.f
+++ vcode.f
@@ -1,6 +1,6 @@
-/home/hxxdev/dev/systemverilog/uvm/src/uvm.sv
-/home/hxxdev/dev/systemverilog/uvm/src/uvm_pkg.sv
+/home/hxxdev/dev/systemverilog/uvm-verilator/src/uvm-verilator.sv
+/home/hxxdev/dev/systemverilog/uvm-verilator/src/uvm-verilator_pkg.sv
 /home/hxxdev/dev/systemverilog/rtl/counter.sv
 /home/hxxdev/dev/systemverilog/tb/top.sv
 /home/hxxdev/dev/systemverilog/tb/uvm_test.sv
--I/home/hxxdev/dev/systemverilog/uvm/src
+-I/home/hxxdev/dev/systemverilog/uvm-verilator/src
